20a62a396b666dc3d699928320a7d7b60015538d
Kosdevua <konstantin.vodolozhko@gmail.com>
2024-06-24 11:16:33 +0300

    some code

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,14 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  const data = await fs.readFile(PATH_DB, 'utf-8');
+  const contactList = JSON.parse(data);
+  const newSongList = Array(number).fill(0).map(createFakeContact);
+  contactList.push(...newSongList);
+  await fs.writeFile(PATH_DB, JSON.stringify(contactList, null, 2));
+  console.log(contactList);
+};
 
 generateContacts(5);
